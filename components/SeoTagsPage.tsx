23caddd7220fb9213119cbf3c61b3b6d92664896
Prince Gupta <beatslevelone@gmail.com>
2026-03-19 17:27:08 +0530

    Update SeoTagsPage.tsx

--- components/SeoTagsPage.tsx
+++ components/SeoTagsPage.tsx
@@ -241,7 +241,7 @@ useEffect(() => {
     };
 
     return (
-        <section className="space-y-6 max-w-5xl mx-auto">
+        <section className="space-y-6 w-full px-2 sm:px-4 md:px-6">
             <ConfirmationModal
                 isOpen={modalConfig.isOpen}
                 onClose={() => setModalConfig({ ...modalConfig, isOpen: false })}
