70091ea0777ca3eb06b53983cac3f9119969bade
Prince Gupta <beatslevelone@gmail.com>
2026-03-19 17:41:21 +0530

    Update MainApplication.tsx

--- components/MainApplication.tsx
+++ components/MainApplication.tsx
@@ -565,7 +565,7 @@ currency={currency}
         quota={userQuota}
       />
 
-      <main className="w-full max-w-7xl mx-auto px-4 pt-20 pb-12">
+      <main className="container mx-auto px-6 pt-20 pb-12">
         {renderView()}
       </main>
     </div>
