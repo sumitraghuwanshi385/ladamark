dde664ec04bc34fae8134294c6203000ce629776
Prince Gupta <beatslevelone@gmail.com>
2026-03-19 16:16:20 +0530

    Update SeoTagsPage.tsx

--- components/SeoTagsPage.tsx
+++ components/SeoTagsPage.tsx
@@ -2,6 +2,12 @@ import React, { useState, useMemo, useEffect, useRef } from 'react';
 import { type LabeledItem, type AiResponseData } from './MainApplication';
 import ConfirmationModal from './ConfirmationModal';
 
+const ChevronDownIcon = ({ className = "w-5 h-5" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+  </svg>
+);
+
 const SeoTagIcon = ({className = "w-6 h-6"}) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
         <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
