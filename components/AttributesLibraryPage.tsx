16925eb5d9fe4f26c069efffc76d1f0f19493901
Prince Gupta <beatslevelone@gmail.com>
2026-03-19 17:26:05 +0530

    Update AttributesLibraryPage.tsx

--- components/AttributesLibraryPage.tsx
+++ components/AttributesLibraryPage.tsx
@@ -332,7 +332,7 @@ const handleExport = () => {
 };
 
     return (
-        <section className="space-y-6 max-w-5xl mx-auto">
+        <section className="space-y-6 w-full px-2 sm:px-4 md:px-6">
              <ConfirmationModal
                 isOpen={modalConfig.isOpen}
                 onClose={() => setModalConfig(prev => ({ ...prev, isOpen: false }))}
